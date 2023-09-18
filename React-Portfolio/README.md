7347e5ce9ed65df3bad3b9866b8b0273e582364d
Kain <kain@MacBook-Pro-van-Kain.local>
2023-09-18 11:38:17 +0200

    Initial project layout

--- React-Portfolio/README.md
+++ React-Portfolio/README.md
@@ -1,10 +1,9 @@
 ### Description
+portfolio
 
-A simple portfolio template for developer/designers built with React. 
+### [live preview](Kainlareine.nl)
 
-### [live preview](https://ubaimutl.github.io/react-portfolio/)
 
-[![react portfoiio](src/assets/images/react%20portfolio%20gif.gif)](https://ubaimutl.github.io/react-portfolio/)
 
 ### Features
 
@@ -16,9 +15,7 @@ A simple portfolio template for developer/designers built with React.
 
 ### Setup
 
-Get the code
 
-<pre>git clone https://github.com/ubaimutl/react-portfolio.git</pre>
  
 Install required dependencies
 
@@ -33,6 +30,3 @@ Start the server
 
 Modify pages content in  `src/content_option.js`.
 
-### Thanks
-
-If you like this portfolio template don't forget give it a ⭐ 
--- React-Portfolio/src/pages/portfolio/index.js
+++ React-Portfolio/src/pages/portfolio/index.js
@@ -5,6 +5,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import { dataportfolio, meta } from "../../content_option";
 
 export const Portfolio = () => {
+  const projectsToShow = dataportfolio.slice(0, 4); // Limit the projects to show
+
   return (
     <HelmetProvider>
       <Container className="About-header">
@@ -15,12 +17,12 @@ export const Portfolio = () => {
         </Helmet>
         <Row className="mb-5 mt-3 pt-md-3">
           <Col lg="8">
-            <h1 className="display-4 mb-4"> Portfolio </h1>{" "}
+            <h1 className="display-4 mb-4"> project </h1>{" "}
             <hr className="t_border my-4 ml-0 text-left" />
           </Col>
         </Row>
         <div className="mb-5 po_items_ho">
-          {dataportfolio.map((data, i) => {
+          {projectsToShow.map((data, i) => {
             return (
               <div key={i} className="po_item">
                 <img src={data.img} alt="" />
